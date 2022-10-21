e203aba15850eb12a2475d7e8b182d8f5a136e76
zsolttakacs2000 <zsolt.takacs2000@gmail.com>
2022-10-21 20:06:08 +0100

    Added unittests as per #8

--- src/mlqda_project/mlqda/tests.py
+++ src/mlqda_project/mlqda/tests.py
@@ -1,3 +1,38 @@
+"""
+Unittesting for mlqda app
+"""
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from mlqda.views import save_file
+
+from os.path import exists
+
+
+class ViewTests(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse('mlqda:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about(self):
+        response = self.client.get(reverse('mlqda:about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact(self):
+        response = self.client.get(reverse('mlqda:contact'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_results(self):
+        response = self.client.get(reverse('mlqda:analyser-results'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Connection')
+        self.assertEqual(len(response.context['topics']), 2)
+        
+    def test_redirect(self):
+        response = self.client.get(reverse('mlqda:analyser-redirect'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_analyser_start(self):
+        response = self.client.get(reverse('mlqda:analyser-start'))
+        self.assertEqual(response.status_code, 200)
+        
